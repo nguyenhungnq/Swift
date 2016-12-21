d36bb75d20c86c26d07a6110c678f1ad0078a928
nguyenhungnq <NguyenHung@Antonio.local>
2016-12-21 19:25:30 +0700

    Example of pushing from Github Desktop
    From Hung with love

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var amICool = true
+
+amICool = !amICool
+
+var feelGoodAboutMyself = true
+
+feelGoodAboutMyself = amICool ? true : false
+
+var bankAccountBalance = 100
+
+var cashRegisterMessage = bankAccountBalance >= 150 ? "You just bought the item" : "You are broke as a joke"
+
